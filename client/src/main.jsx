b6cf71759ae94819858d74ed8775cc2bf54e1e82
lup62 <132756055+lup62@users.noreply.github.com>
2026-06-19 20:29:57 +0200

    pulizia css di default 2

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import {ThemeProvider} from "@mui/material/styles";
 import theme from "./theme/theme.js";
+import "./index.css"
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
