d543fd247c6e628dd4710777fd5ea24831b334db
lup62 <132756055+lup62@users.noreply.github.com>
2026-06-19 19:49:37 +0200

    implementazione iniziale di MUI

--- client/src/layouts/MainLayout.jsx
+++ client/src/layouts/MainLayout.jsx
@@ -1,20 +1,32 @@
-import {Outlet} from "react-router-dom"
-import Navbar from "../components/Navbar"
+import { Outlet } from "react-router-dom";
+import { Box, Container } from "@mui/material";
+import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
-function MainLayout(){
+function MainLayout() {
     return (
-        <div>
-            <header>
-                <Navbar />
-            </header>
-            <main>
+        <Box
+            sx={{
+                minHeight: "100vh",
+                display: "flex",
+                flexDirection: "column",
+            }}
+        >
+            <Navbar />
+
+            <Container
+                component="main"
+                maxWidth="lg"
+                sx={{
+                    flex: 1,
+                    py: 4,
+                }}
+            >
                 <Outlet />
-            </main>
-            <footer>
-                <Footer />
-            </footer>
-        </div>
+            </Container>
+
+            <Footer />
+        </Box>
     );
 }
 
