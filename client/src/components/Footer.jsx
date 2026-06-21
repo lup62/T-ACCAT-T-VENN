2eee0f46938fd055336092691047262a24f6a7da
lup62 <132756055+lup62@users.noreply.github.com>
2026-06-21 18:58:21 +0200

    spaziatura footer e piccola scritta

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 
 function Footer() {
     return (
@@ -12,9 +12,24 @@ function Footer() {
                 mt: "auto",
             }}
         >
-            <Typography variant="body2">
-                © 2026 T&apos;ACCAT & T&apos;VENN
-            </Typography>
+            <Container
+                maxWidth="lg"
+                sx={{
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "space-between",
+                    flexWrap: "wrap",
+                    gap: 1,
+                }}>
+                <Typography variant="body2">
+                    © 2026 T&apos;ACCAT & T&apos;VENN
+                </Typography>
+
+                <Typography variant="body2">
+                    Fatto con amore ♡ da Oronzo, Pasquale e Giovanni
+                </Typography>
+            </Container>
+
         </Box>
     );
 }
