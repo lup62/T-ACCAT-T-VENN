0024a24bdd6a845ed26c7590d2443dd265b62e8d
lup62 <132756055+lup62@users.noreply.github.com>
2026-07-01 11:33:18 +0200

    aggiunto UseAuth allineato con il backend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,12 +5,15 @@ import {ThemeProvider} from "@mui/material/styles";
 import theme from "./theme/theme.js";
 import "./index.css"
 import App from './App.jsx'
+import { AuthProvider } from "./contexts/AuthContext.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <ThemeProvider theme={theme}>
           <BrowserRouter>
-              <App />
+              <AuthProvider>
+                  <App />
+              </AuthProvider>
           </BrowserRouter>
       </ThemeProvider>
   </StrictMode>,
