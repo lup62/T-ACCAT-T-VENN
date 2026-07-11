0f2a8cb31fa35094b0fb5d11c46b853bdbcfa83a
GiovanniPastore10 <g.pastore15@studenti.poliba.it>
2026-07-11 15:50:36 +0200

    Aggiunta conclusione annunci
    Aggiunta route PATCH /api/annunci/:id/concludi protetta da JWT.

--- server/routes/annuncioRoutes.js
+++ server/routes/annuncioRoutes.js
@@ -6,6 +6,7 @@ const {
     dettaglioAnnuncio,
     modificaAnnuncio,
     chiudiAnnuncio,
+    concludiAnnuncio,
 } = require("../controllers/annuncioController");
 const router = express.Router();
 
@@ -14,6 +15,7 @@ router.get("/", listaAnnunci);
 router.get("/:id", dettaglioAnnuncio);
 router.post("/", requireAuth, creaAnnuncio);
 router.patch("/:id/chiudi", requireAuth, chiudiAnnuncio);
+router.patch("/:id/concludi", requireAuth, concludiAnnuncio);
 router.patch("/:id", requireAuth, modificaAnnuncio);
 
 
